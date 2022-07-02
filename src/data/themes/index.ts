47393467d8e6f4a39a2acaa376fbfb57b772fc3f
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-07-02 18:13:23 +1000

    Update index.ts

--- src/data/themes/index.ts
+++ src/data/themes/index.ts
@@ -1,27 +1,16 @@
-import frostedglass from './frostedglass';
-import softx from './softx';
-import basicbackground from './basicbackground';
-import clearvision from './clearvision';
-import reborn from './reborn';
-import tranquil from './tranquil';
-import discordrecolor from './discordrecolor';
-import comfy from './comfy';
-import solana from './solana';
-import materialdesign from './materialdesign';
+import frostedglass from "./frostedglass";
+import softx from "./softx";
+import basicbackground from "./basicbackground";
+import clearvision from "./clearvision";
+import reborn from "./reborn";
+import tranquil from "./tranquil";
+import discordrecolor from "./discordrecolor";
+import comfy from "./comfy";
+import solana from "./solana";
+import materialdesign from "./materialdesign";
 
-const themes = [
-	frostedglass,
-	softx,
-	basicbackground,
-	clearvision,
-	reborn,
-	tranquil,
-	discordrecolor,
-	comfy,
-	solana,
-	materialdesign,
-]
+const themes = [frostedglass, softx, basicbackground, clearvision, reborn, tranquil, discordrecolor, comfy, solana, materialdesign];
 
-export const fallbacks = themes.map(el => el.name.replace(/ /g, '').toLowerCase());
+export const fallbacks = themes.map((el) => el.name.replace(/ /g, "").toLowerCase());
 
 export default themes;
