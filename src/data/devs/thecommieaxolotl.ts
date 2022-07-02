2c4881e86379586a512f7f745b9c4c14250e4a03
TheCommieAxolotl <87679354+TheCommieAxolotl@users.noreply.github.com>
2022-07-02 18:11:10 +1000

    remove tag

--- src/data/devs/thecommieaxolotl.ts
+++ src/data/devs/thecommieaxolotl.ts
@@ -1,10 +1,9 @@
-import type {IDev} from "$types/dev";
+import type { IDev } from "$types/dev";
 
 const dev: IDev = {
-    name: 'TheCommieAxolotl',
-    tag: 0o1,
-    github: 'TheCommieAxolotl',
-    donate: 'https://github.com/sponsors/TheCommieAxolotl/'
+    name: "TheCommieAxolotl",
+    github: "TheCommieAxolotl",
+    donate: "https://github.com/sponsors/TheCommieAxolotl/",
 };
 
 export default dev;
