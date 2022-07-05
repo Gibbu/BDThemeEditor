ab391ca56495e5cee373522a792a7660e8d4fb74
maenDisease <ninomae2n@gmail.com>
2022-07-04 23:28:55 -0400

    Extended customization

--- src/data/themes/solana.ts
+++ src/data/themes/solana.ts
@@ -76,10 +76,21 @@ const theme: ITheme = {
 				{
 					type: 'slider',
 					details: {
-						variable: 'text-link-hue',
-						value: 212,
-						max: 359,
-						title: 'Text link hue'
+						variable: 'background-accent-saturation',
+						value: 100,
+						max: 100,
+						unit: '%',
+						title: 'Background accent saturation'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'background-accent-brightness',
+						value: 50,
+						max: 100,
+						unit: '%',
+						title: 'Background accent brightness'
 					}
 				}
 			]
@@ -96,17 +107,6 @@ const theme: ITheme = {
 						value: '',
 						title: 'Background image',
 						comment: 'url(https://site.com/foobar.png) | URL MUST BE A DIRECT LINK (ending in .jpg, .jpeg, .png, .gif)'
-					}
-				},
-				{
-					type: 'slider',
-					details: {
-						variable: 'background-image-blur',
-						value: 0,
-						max: 50,
-						unit: 'px',
-						title: 'Background image blur',
-						comment: 'Blur intensity of --background-image'
 					}
 				},
 				{
@@ -147,6 +147,16 @@ const theme: ITheme = {
 				{
 					type: 'slider',
 					details: {
+						variable: 'background-image-blur',
+						value: 0,
+						max: 50,
+						unit: 'px',
+						title: 'Background image blur'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
 						variable: 'background-overlay-opacity',
 						value: 80,
 						max: 100,
@@ -157,38 +167,85 @@ const theme: ITheme = {
 				{
 					type: 'slider',
 					details: {
+						variable: 'background-overlay-color-lightness',
+						value: 0,
+						min: -1,
+						max: 1,
+						step: 0.01,
+						title: 'Overlay lightness'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
 						variable: 'background-hue-shift-strength',
 						value: 100,
 						max: 100,
 						unit: '%',
-						title: 'Background hue-shift strength'
+						title: 'Background hue-shift strength',
+						hint: 'Disabled if background image is set.'
 					}
 				}
 			]
 		},
 		{
 			title: 'Window',
-			icon: 'App',
+			icon: 'Sizing',
 			inputs: [
 				{
-					type: 'slider',
+					type: 'image',
 					details: {
-						variable: 'window-margin',
-						value: 24,
-						max: 100,
-						step: 1,
-						title: 'Window padding',
-						hint: 'Spacing around the window'
+						variable: 'window-background-image',
+						start: 'initial',
+						value: '',
+						title: 'Window background image'
 					}
 				},
 				{
-					type: 'slider',
+					type: 'select',
 					details: {
-						variable: 'window-accent-tint-strength',
-						value: 5,
-						max: 100,
-						unit: '%',
-						title: 'Window tint strength'
+						variable: 'window-background-image-position',
+						value: 'center',
+						options: [
+							{ label: 'Top left', value: 'top left' },
+							{ label: 'Top centre', value: 'top center' },
+							{ label: 'Top right', value: 'top right' },
+							{ label: 'Centre left', value: 'center left' },
+							{ label: 'Centre', value: 'center' },
+							{ label: 'Centre right', value: 'center right' },
+							{ label: 'Bottom left', value: 'Bottom left' },
+							{ label: 'Bottom centre', value: 'Bottom center' },
+							{ label: 'Bottom right', value: 'bottom right' }
+						],
+						title: 'Position'
+					}
+				},
+				{
+					type: 'select',
+					details: {
+						variable: 'window-background-image-size',
+						value: 'cover',
+						options: [
+							{ label: 'Cover', value: 'cover' },
+							{ label: 'Contain', value: 'contain' },
+							{ label: 'Auto', value: 'auto' },
+							{ label: '100%', value: '100%' },
+							{ label: '150%', value: '150%' },
+							{ label: '200%', value: '200%' }
+						],
+						title: 'Size'
+					}
+				},
+				{
+					type: 'select',
+					details: {
+						variable: 'window-background-image-attachment',
+						value: 'fixed',
+						options: [
+							{ label: 'Fixed', value: 'fixed' },
+							{ label: 'Local', value: 'local' }
+						],
+						title: 'Attachment'
 					}
 				},
 				{
@@ -198,7 +255,8 @@ const theme: ITheme = {
 						value: 100,
 						max: 100,
 						unit: '%',
-						title: 'Window opacity'
+						title: 'Window opacity',
+						hint: 'Reduce opacity to show background image.'
 					}
 				},
 				{
@@ -207,10 +265,40 @@ const theme: ITheme = {
 						variable: 'window-blur',
 						value: 0,
 						max: 50,
-						unit: 'px',
 						step: 1,
 						title: 'Window blur',
-						hint: 'Blur behind the window'
+						hint: 'Blur behind the window.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'window-friends-blur',
+						value: 0,
+						max: 50,
+						step: 1,
+						title: 'Friends page blur'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'window-margin',
+						value: 24,
+						max: 100,
+						step: 1,
+						title: 'App padding',
+						hint: 'Spacing around the window.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'window-margin-left',
+						value: 0,
+						max: 100,
+						step: 1,
+						title: 'App padding left',
 					}
 				},
 				{
@@ -222,43 +310,72 @@ const theme: ITheme = {
 						unit: 'px',
 						step: 1,
 						title: 'Window rounded',
-						hint: 'Roundness of the window'
-					}
-				}
-			]
-		},
-		{
-			title: 'Floating background',
-			icon: 'Colour',
-			inputs: [
-				{
-					type: 'slider',
-					details: {
-						variable: 'background-floating-blur',
-						value: 20,
-						max: 50,
-						unit: 'px',
-						step: 1,
-						title: 'Floating background blur',
-						hint: 'Blur behind the floating background'
+						hint: 'Roundness of the window.'
 					}
 				},
 				{
-					type: 'select',
+					type: 'slider',
 					details: {
-						variable: 'enable-floating-transparency',
-						value: '1',
-						options: [
-							{ label: 'True', value: '1' },
-							{ label: 'False', value: '0' }
-						],
-						title: 'Enable floating transparency'
+						variable: 'window-background-lightness',
+						value: 0,
+						min: -1,
+						max: 1,
+						step: 0.01,
+						title: 'Window background lightness'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'window-accent-tint-strength',
+						value: 5,
+						max: 100,
+						unit: '%',
+						title: 'Window tint strength'
 					}
 				}
 			]
 		},
 		{
-			title: 'Home icon',
+			title: 'Chat',
+			icon: 'Chat',
+			inputs: [
+				{
+					type: 'select',
+					details: {
+						variable: 'chat-bubble',
+						value: '0',
+						options: [
+							{ label: 'False', value: '0' },
+							{ label: 'True', value: '1' }
+						],
+						title: 'Chat bubbles'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'text-link-hue',
+						value: 212,
+						max: 359,
+						title: 'Text link hue'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'chat-text-brightness',
+						value: 0,
+						min: -1,
+						max: 1,
+						step: 0.01,
+						title: 'Chat text brightness'
+					}
+				}
+			]
+		},
+		{
+			title: 'Dock',
 			icon: 'Home',
 			inputs: [
 				{
@@ -277,6 +394,175 @@ const theme: ITheme = {
 						start: 'initial',
 						value: '',
 						title: 'Favorites button image'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'dock-blur',
+						value: 0,
+						max: 50,
+						title: 'Dock blur',
+						comment: 'Blur intensity of the dock.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'dock-border-radius',
+						value: 0,
+						min: -1,
+						max: 1,
+						step: 0.01,
+						title: 'Dock radius'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'guildlist-show',
+						value: 1,
+						max: 1,
+						step: 0.01,
+						title: 'Auto-collapse icons',
+						hint: 'The icons will reappear on hover.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'guildlist-peek-unread',
+						value: 1,
+						max: 1,
+						step: 0.01,
+						title: 'Peek unread',
+						hint: 'Partially show when collapsed.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'guildlist-peek-ping',
+						value: 1,
+						max: 1,
+						step: 0.01,
+						title: 'Peek unread',
+						hint: 'Partially show when collapsed.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'guildlist-peek-selected',
+						value: 1,
+						max: 1,
+						step: 0.01,
+						title: 'Peek unread',
+						hint: 'Partially show when collapsed.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'guildlist-peek-group-unread',
+						value: 1,
+						max: 1,
+						step: 0.01,
+						title: 'Peek unread folder',
+						hint: 'Partially show when collapsed.'
+					}
+				},
+			]
+		},
+		{
+			title: 'Sidebar',
+			icon: 'List',
+			inputs: [
+				{
+					type: 'slider',
+					details: {
+						variable: 'sidebar-width',
+						value: 240,
+						min: 48,
+						max: 480,
+						unit: 'px',
+						step: 1,
+						title: 'Sidebar width'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'members-width',
+						value: 240,
+						min: 64,
+						max: 480,
+						unit: 'px',
+						step: 1,
+						title: 'Members width'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'sidebar-text-brightness',
+						value: 0,
+						min: -1,
+						max: 1,
+						step: 0.01,
+						title: 'Sidebar text brightness'
+					}
+				}
+			]
+		},
+		{
+			title: 'Floating background',
+			icon: 'Border',
+			inputs: [
+				{
+					type: 'slider',
+					details: {
+						variable: 'background-floating-blur',
+						value: 20,
+						max: 50,
+						unit: 'px',
+						step: 1,
+						title: 'Floating background blur',
+						hint: 'Blur behind the floating background.'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'floating-background-lightness',
+						value: 0,
+						min: -1,
+						max: 1,
+						step: 0.01,
+						title: 'Floating background lightness'
+					}
+				},
+				{
+					type: 'slider',
+					details: {
+						variable: 'background-floating-opacity',
+						value: 1,
+						min: 0,
+						max: 1,
+						step: 0.01,
+						title: 'Floating background opacity'
+					}
+				},
+				{
+					type: 'select',
+					details: {
+						variable: 'enable-floating-transparency',
+						value: '1',
+						options: [
+							{ label: 'True', value: '1' },
+							{ label: 'False', value: '0' }
+						],
+						title: 'Enable floating transparency'
 					}
 				}
 			]
@@ -300,7 +586,7 @@ const theme: ITheme = {
 					details: {
 						variable: 'custom-font-regular',
 						start: '""',
-						index: 1,
+						index: 0,
 						value: '',
 						title: 'Regular font',
 						hint: 'Like the primary font, but used in specific areas.'
@@ -311,7 +597,7 @@ const theme: ITheme = {
 					details: {
 						variable: 'custom-font-primary',
 						start: '""',
-						index: 2,
+						index: 0,
 						value: '',
 						title: 'Primary font'
 					}
@@ -321,7 +607,7 @@ const theme: ITheme = {
 					details: {
 						variable: 'custom-font-code',
 						start: '""',
-						index: 3,
+						index: 0,
 						value: '',
 						title: 'Code font'
 					}
@@ -342,7 +628,7 @@ const theme: ITheme = {
 							{ label: 'False', value: '0' }
 						],
 						title: 'Enable blur',
-						hint: 'Forces blur variables to 0'
+						hint: 'Forces blur variables to 0.'
 					}
 				}
 			]
