0597ffa192267f10f125e1c348b2b604224c9b67
maenDisease <ninomae2n@gmail.com>
2023-02-21 22:32:56 -0500

    updated spectra and neutron

--- src/data/devs/spectra.ts
+++ src/data/devs/spectra.ts
@@ -2,7 +2,7 @@ import type {IDev} from '$types/dev';
 
 const dev: IDev = {
 	name: 'Spectra',
-	github: 'codedotspectra',
+	github: 'xcruxiex',
 	banner: 'https://cdn.discordapp.com/banners/286208664623120384/91ab39c81aae3abf1b087d07df6b489b.png?size=600',
 	colour: 'hsl(359 96.3% 42.7%)',
 	server: 'https://discord.gg/PZdnCVD'
