2444013810e49b10dcf7041be04cb2259a8f6b22
Ian <Gibbu_@hotmail.com>
2023-03-13 12:05:56 +1100

    Updated FrostedGlass default font

--- src/data/themes/frostedglass.ts
+++ src/data/themes/frostedglass.ts
@@ -49,7 +49,7 @@ const theme: ITheme = {
 			--window-roundness: 10px; /* Roundness of Discord | DEFAULT: 10px */
 			--scrollbar-colour: rgba(255, 255, 255, 0.05); /* DEFAULT: rgba(255,255,255,0.05) */
 			--link-colour: #00b0f4; /* DEFAULT: #00b0f4 */
-			--font: "Whitney";
+			--font: "gg sans";
 			--update-notice-1: none;
 		}
 	`,
@@ -458,7 +458,7 @@ const theme: ITheme = {
 					details: {
 						variable: 'font',
 						index: 0,
-						value: 'Whitney',
+						value: 'gg sans',
 						title: 'App font'
 					}
 				}
