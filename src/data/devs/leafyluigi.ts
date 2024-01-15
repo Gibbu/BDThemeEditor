e801c42e7c5558a24b86014494f56ba49e2584bd
LeafyLuigi <bw2154@gmail.com>
2024-01-15 13:57:06 +0000

    add self as dev
    (from previous commit) The font file was changed to include the ability to use `hint`.

---
+++ src/data/devs/leafyluigi.ts
@@ -0,0 +1,10 @@
+import type { Developer } from '$types/dev';
+
+const dev: Developer = {
+	name: 'LeafyLuigi',
+	github: 'LeafyLuigi',
+	colour: '219 100% 61%',
+	server: 'https://discord.gg/7P99YTBRUu'
+};
+
+export default dev;
