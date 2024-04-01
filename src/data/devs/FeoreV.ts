4ed27fcbf2efb7178558a40bb32790cdefb3da0d
FeoreV <81682869+FeoreV@users.noreply.github.com>
2024-04-02 00:22:07 +0500

    Add files via upload

---
+++ src/data/devs/FeoreV.ts
@@ -0,0 +1,11 @@
+import type { Developer } from '$types/dev';
+
+const dev: Developer = {
+	name: 'FeoreV',
+	github: 'FeoreV',
+	colour: '170 74% 45%',
+	banner: 'https://wallpapersmug.com/download/2048x1152/f1bd77/black-angel-cute-anime-girl-art.jpg',
+	server: 'https://discord.gg/baEMQkgswT'
+};
+
+export default dev;
