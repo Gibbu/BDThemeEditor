933ee9300d2ede70228da534d218cf9aeb48ad36
FeoreV <81682869+FeoreV@users.noreply.github.com>
2024-04-17 21:46:55 +0500

    Update materialwin.ts

--- src/data/addons/FVUI/materialwin.ts
+++ src/data/addons/FVUI/materialwin.ts
@@ -3,7 +3,7 @@ import FeoreV from '$data/devs/FeoreV';
 
 const addon: Addon = {
 	name: 'Addon - MaterialWin',
-	description: 'The theme color scheme adjusts to the windows colorscheme color. Works only in BetterDiscord',
+	description: 'The theme color scheme adjusts to the windows colorscheme color',
 	developer: FeoreV,
 	imports: ['https://feorev.github.io/Themes/Discord/FVUI/Addons/MaterialWin.css'],
 	previewUrl: ['https://feorev.github.io/Themes/Discord/FVUI/Addons/MaterialWin.css'],
